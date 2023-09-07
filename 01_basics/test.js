5a3f2d46594359ea3e066bca028f0e0089edc2d7
Shubham Verma <94617569+shubhambbk50@users.noreply.github.com>
2023-09-07 14:06:23 +0000

    a

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("krishna")
+const accID = 100
+let accEmail = "myemail@gmail.com"
+var accPassword = "12345"
+accCity = "Delhi"
+let accState;
+
+// accID = 110 //  not allowed
+
+accEmail = "youremail@gmail.com"
+accPassword = "567890"
+accCity = "Mumbai"
+
+/* prefer not to use var 
+    since it scope problems */
+
+console.log(accID);
+console.table([accID, accEmail, accPassword, accCity, accState]);

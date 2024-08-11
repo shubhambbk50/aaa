1cfc0bbd015c8a4cfe1c6816cffb1a7d26edd81f
shubham <shubhambbk50@gmail.com>
2024-08-11 09:39:15 +0530

    update

---
+++ string.js
@@ -0,0 +1,86 @@
+// DATATYPES
+// 7 primitive
+// Number
+// String
+// Boolean
+// undefined
+// null
+// Object
+// Symbol
+// BigInt
+
+// non primitive
+// Object: 
+// const person = { name: "John", age: 30 };
+// Array
+// const numbers = [1, 2, 3, 4];
+// Function
+// function greet() { console.log("Hello!"); }
+// Date
+// const today = new Date();
+
+
+// stack memory allocation, heap memory allocation 
+
+
+
+
+
+
+// string concatenation
+
+// use backtick not single quote for concatenation
+// const userName = "alexander"; // variable name best way -> small + 1 CAPITAL + small
+// const repoCount = 50;
+// console.log(`Hello my name is ${userName} and my repo are ${repoCount}`);
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+// console.log(userName.charAt(1))
+// console.log(userName.indexOf('c'));
+
+// const newString = userName.substring(0,5)
+// console.log(newString)
+
+// const anotherString = userName.slice(-8,-1)  //positive values work similar to substring, but negative from abakc
+// console.log(anotherString)
+
+// print 
+// console.log(5);
+
+// alert("hello");   display elert in browser
+
+// document.write() is used when you want to print the content to the HTML document.
+// document.write("hello");
+
+// const inp1 = prompt("enter"); similar to alert dialog-box , it takes Input
+// const inp1 = parseInt(prompt("enter"));  it converts input string into number
+
+// console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+// const area = function () {
+//     console.log(length*breadth);
+// }
+// const length = 5;
+// const breadth = 2;
+// area();
+
+
+
+// //Ja/vaScript program to swap two variables
+
+// //take input from the users
+// let a = prom0pt('Enter the first variable: ');
+ // let b = prompt('Enter the second variable: ');
+
+// /// XOR operator
+// a = a ^ b
+// b = a ^ b
+// a = a ^ b
+
+// console.log(`The value of a after swapping: ${a}`);
+// console.log(`The value of b after swapping: ${b}`);
+
+
+
